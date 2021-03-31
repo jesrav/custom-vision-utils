b8f785514d73d09daba5e5271df4ac07f2cd6a79
jesravnbol <jra@kapacity.dk>
2021-03-31 16:49:56 +0200

    Added export model fuctionality to cli.

--- custom_vision_utils/cli/cli.py
+++ custom_vision_utils/cli/cli.py
@@ -3,6 +3,7 @@ import click
 from custom_vision_utils.cli.upload_images import upload_images
 from custom_vision_utils.cli.create_projects import create_projects
 from custom_vision_utils.cli.train import train
+from custom_vision_utils.cli.export_model import export_model
 
 
 @click.group()
@@ -13,7 +14,7 @@ def cli():
 cli.add_command(create_projects)
 cli.add_command(upload_images)
 cli.add_command(train)
-
+cli.add_command(export_model)
 
 
 if __name__ == "__main__":
---
+++ custom_vision_utils/cli/export_model.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+import click
+from dotenv import load_dotenv, find_dotenv
+
+from custom_vision_utils.sdk_helpers import (
+    get_trainer,
+    download_model_iteration_as_tensorflow,
+    get_iteration_id,
+)
+from custom_vision_utils.cli.logger import logger
+
+
+@click.command()
+@click.argument("project_name", type=str)
+@click.argument("outpath", type=str)
+@click.option("--iteration_name", type=str)
+def export_model(project_name, outpath, iteration_name):
+
+    # Loading environment variables from .env file
+    load_dotenv(find_dotenv())
+
+    trainer = get_trainer()
+    if iteration_name:
+        iteration_id = get_iteration_id(trainer, project_name, iteration_name)
+    else:
+        iteration_id = None
+        iteration_name = "latest"
+
+    logger.info(
+        f"Downloading model artifacts for project {project_name}, "
+        f"iteration: {iteration_name} to {outpath}"
+    )
+
+    download_model_iteration_as_tensorflow(project_name, Path(outpath), iteration_id)
+
