06b48bc9def6667f9d2159dde3237cfe80d97165
jesravnbol <jra@kapacity.dk>
2021-04-01 12:10:49 +0200

    container_name in image interface. Implemented as dummy in local image classes.

--- custom_vision_utils/image/image_interface.py
+++ custom_vision_utils/image/image_interface.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Union
 
 
 class ImageInterface(ABC):
@@ -6,6 +8,16 @@ class ImageInterface(ABC):
     @abstractmethod
     def get_pil_image(self):
         pass
+
+    @staticmethod
+    @abstractmethod
+    def from_azure_custom_vision_image(
+            custom_vision_image,
+            folder: Union[Path, str],
+            container
+    ):
+        pass
+
 
 
 
